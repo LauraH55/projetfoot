1d13ecb1bf42f20fd672ca27109647071fb65f2a
Laura <laura.hantz@hotmail.fr>
2019-11-26 10:34:17 +0100

    Vues équipe (nom, date fondation, président, logo)

--- team.php
+++ team.php
@@ -14,13 +14,23 @@ $stmt->execute();
 $team = $stmt->fetch();
 
 $date = new DateTime($team['fundation_date']);
-echo $date->format('d-m-Y');
 
 
 ?>
 
-<?= $team['name']; ?>
-<?= $team['fundation_date']; ?>
-<?= $team['president']; ?>
+
+<div class="container description" style="color: black;">
+  <div class="row">
+    <div class="col-md-8">
+      <h1><?php echo $team['name']; ?></h1>
+      <img src="<?php echo $team['logo'];?>" alt="">
+      <p>Nom : <?php echo $team['name']; ?> </p>
+      <p>Fondé en : <?php echo $date->format('d/m/Y'); ?> </p>
+      <p>Président : <?php echo $team['president']; ?></p>
+    </div>
+  </div>
+</div>
+
+
 
 <?php include('include/footer.php') ?>
